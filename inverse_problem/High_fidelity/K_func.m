886cb681044780b9a44a77af102cef83a5927005
shamsbasir <shamsbasir@gmail.com>
2022-05-19 19:36:28 -0400

    updated

--- inverse_problem/High_fidelity/K_func.m
+++
@@ -1,4 +0,0 @@
-function K = K_func(S_e)
-global K_s m
-K    = K_s*S_e.^(1/2).*(1 - (1 - S_e.^(1/m)).^m).^2;
-return 
--- inverse_problem/Low_fidelity/guess.m
+++
@@ -1,4 +0,0 @@
-function g = guess(x) % initial guess for y and y'
-g = [0.0
-     0.0];
-end
